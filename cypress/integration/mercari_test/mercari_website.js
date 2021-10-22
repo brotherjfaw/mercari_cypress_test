3f0d0e6b588871f4c7cb97d2b9e21d8b0c9c13da
Jahi <brotherjfaw@gmail.com>
2021-10-21 22:05:28 -0400

    Mercari Web testing cypress scripts

---
+++ cypress/integration/mercari_test/mercari_website.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress"/>
+
+describe('Test Mercari Site using Cypress', ()=>{
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Confirm Shopping Cart', ()=>{
+        cy.get('.components__WrapperButton-sc-18pafoj-0 > .Flex-ych44r-0 > svg').click()
+        cy.contains('Your shopping cart is empty')
+    })
+    it('Search for Shirts', ()=>{
+        cy.get('[data-testid=SearchInput]').type('Shirts')
+        cy.get('[data-testid=SearchIcon] > svg').click()
+    })
+    it('Confirm Login requires correct credentials', ()=>{
+        cy.get('[data-testid=LoginButton]').click()
+        cy.get('[data-testid=EmailInput]').type('brotherjfaw@gmail.com')
+        cy.get('[data-testid=PasswordInput]').type('password00')
+        cy.get('[data-testid=LoginSubmitButton] > .Text-uqn6ov-0').click()
+        cy.contains('Something went wrong.')
+    })
+})
